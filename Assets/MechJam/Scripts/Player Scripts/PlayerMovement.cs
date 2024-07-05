1ec6ff0e70cb3565e1f5322cad7844d52afdd041
Ellipsis-sudo <brandon.nuguid18@gmail.com>
2024-07-05 22:55:15 +0800

    Added movement to the player

--- Assets/MechJam/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/MechJam/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,10 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Rigidbody2D rb;
+
+    [Range(0f, 10f)]
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] NanoBotInputs nanobotInputs;
+
+    Vector2 moveDirection = Vector2.zero;
+
+    private InputAction move;
+    
+    private void OnEnable()
+    {
+        move = nanobotInputs.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
+
+    private void Awake()
+    {
+        nanobotInputs = new NanoBotInputs();
+    }
     void Start()
     {
         
@@ -13,6 +39,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, 0);
     }
 }
