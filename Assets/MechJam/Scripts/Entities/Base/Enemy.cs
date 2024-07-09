52eea1053fd092e207a89dbaf62acc81183a0106
Michael Medrano <44107228+mrmedrano81@users.noreply.github.com>
2024-07-09 16:02:18 -0700

    Adding state machines

---
+++ Assets/MechJam/Scripts/Entities/Base/Enemy.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamageable, IEnemyMoveable
+{
+    public float maxHealth { get ; set; }
+    public float currentHealth { get ; set ; }
+    public Rigidbody2D RB { get ; set ; }
+    public bool isFacingRight { get; set; } = false;
+
+    #region State Machine Variables
+
+    public EnemyStateMachine StateMachine { get; set; }
+    public EnemyIdleState IdleState { get; set; }
+    public EnemyChaseState ChaseState { get; set; }
+    public EnemyAttackState AttackState { get; set; }
+
+    #endregion
+
+    #region Idle Variables
+
+    public float RandomMovementRange = 5f;
+    public float RandomMovementSpeed = 1f;
+
+    #endregion
+
+    private void Awake()
+    {
+        StateMachine = new EnemyStateMachine();
+
+        IdleState = new EnemyIdleState(this, StateMachine);
+        ChaseState = new EnemyChaseState(this, StateMachine);
+        AttackState = new EnemyAttackState(this, StateMachine);
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+
+        RB = GetComponent<Rigidbody2D>();
+
+        StateMachine.Initialize(IdleState);
+    }
+    private void Update()
+    {
+        StateMachine.CurrentEnemyState.FrameUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        StateMachine.CurrentEnemyState.PhysicsUpdate();
+    }
+
+    public void Damage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+    }
+
+    public void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    #region Movement
+    public void MoveEnemy(Vector2 velocity)
+    {
+        RB.velocity = velocity;
+        CheckForLeftOrRightFacing(velocity);
+    }
+
+    public void CheckForLeftOrRightFacing(Vector2 velocity)
+    {
+        if (isFacingRight && velocity.x < 0f)
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = !isFacingRight;
+        }
+        else if (!isFacingRight && velocity.x >0f)
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = !isFacingRight;
+        }
+    }
+    #endregion
+
+    #region Animation Triggers
+
+    private void AnimationTriggerEvent(AnimationTriggerType triggerType)
+    {
+        StateMachine.CurrentEnemyState.AnimationTriggerEvent(triggerType);
+    }
+
+    public enum AnimationTriggerType
+    {
+        EnemyAttacking,
+        PlayFootstepSound
+    }
+
+    #endregion
+}
